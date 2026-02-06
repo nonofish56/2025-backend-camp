34b3dddd3905e091c865db621ab78d3521b8ba8b
nonofish56 <nono70434@gmail.com>
2026-02-06 22:40:36 +0800

    確認第七關

--- backend/app.js
+++ backend/app.js
@@ -27,7 +27,7 @@ app.use(pinoHttp({
 }))
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get("/healthcheck", (req, res) => {
+app.get('/healthcheck', (req, res) => {
   res.status(200).json({
   status: 'success',
   message: 'OK'
